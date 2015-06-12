a69742c7fdaa3a89cc6acda4e13ec2c35922c074
Laurent GAY <l.gay@sd-libre.fr>
2015-06-12 15:02:57 +0200

    Change captcha key with new ask to google

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld_GcQSAAAAAAhIvwcgalcQAPEJLrDDrNS17pAt'
-  config.private_key = '6Ld_GcQSAAAAAIeIaUTrf33OwqK5-Ltpl5JWWRcL'
+  config.public_key  = '6LdTQwgTAAAAAAetQmnvof4olU8_UJvFEmveXpoF'
+  config.private_key = '6LdTQwgTAAAAAASBv4U9yumwdSTx_8zOFjnkIVM8'
 end
 
